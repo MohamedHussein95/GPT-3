fbfcab0951c469ba5d4869e2e58fc7b8a3e247d7
Mohamed Hussein <abdikafimohamed441@gmail.com>
2023-07-23 20:45:01 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {
+  Blog,
+  Feature,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+import { Cta, Brand, Navbar } from "./components";
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
